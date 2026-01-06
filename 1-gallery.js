6d307046836c25a37ef2f076f4d8f26f263495c0
beolly <134434470+beolly@users.noreply.github.com>
2026-01-06 11:00:34 +0000

    Deploying to gh-pages from @ beolly/goit-js-hw-09@643c273b6b2c339acd38d3d8e22d1382b16b6fb9 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const s=[{preview:"https://picsum.photos/id/1011/300/200",original:"https://picsum.photos/id/1011/1200/800",description:"Mountain landscape"},{preview:"https://picsum.photos/id/1025/300/200",original:"https://picsum.photos/id/1025/1200/800",description:"Cute dog"},{preview:"https://picsum.photos/id/1062/300/200",original:"https://picsum.photos/id/1062/1200/800",description:"Sea coast"}],p=document.querySelector(".gallery"),a=s.map(({preview:i,original:t,description:o})=>`
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as s}from"./assets/vendor-CgTBfC_f.js";const p=[{preview:"https://picsum.photos/id/1011/300/200",original:"https://picsum.photos/id/1011/1200/800",description:"Mountain landscape"},{preview:"https://picsum.photos/id/1025/300/200",original:"https://picsum.photos/id/1025/1200/800",description:"Cute dog"},{preview:"https://picsum.photos/id/1062/300/200",original:"https://picsum.photos/id/1062/1200/800",description:"Sea coast"}],a=document.querySelector(".gallery"),e=p.map(({preview:i,original:t,description:o})=>`
     <li class="gallery-item">
       <a class="gallery-link" href="${t}">
         <img
@@ -8,5 +8,5 @@ import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                
         />
       </a>
     </li>
-  `).join("");p.innerHTML=a;new SimpleLightbox(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250,captionPosition:"bottom"});
+  `).join("");a.innerHTML=e;new s(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250,captionPosition:"bottom"});
 //# sourceMappingURL=1-gallery.js.map
